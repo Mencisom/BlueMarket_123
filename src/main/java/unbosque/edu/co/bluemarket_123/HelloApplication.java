2098d1b86a30b0add0b18616193c34d5c2bc8399
mencisom@unbosque.edu.co <100002319+Mencisom@users.noreply.github.com>
2022-05-17 17:45:57 -0500

    logic

---
+++ src/main/java/unbosque/edu/co/bluemarket_123/HelloApplication.java
@@ -0,0 +1,9 @@
+package unbosque.edu.co.bluemarket_123;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ src/main/java/unbosque/edu/co/bluemarket_123/HelloResource.java
@@ -0,0 +1,14 @@
+package unbosque.edu.co.bluemarket_123;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
